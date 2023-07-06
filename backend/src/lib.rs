dd20c24e7d957ce69b1524cbba50f9b5174f7567
mwombeki <hackerlubex@protonmail.com>
2023-07-06 23:57:17 +0300

    commit

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -3,3 +3,10 @@ pub mod settings;
 pub mod startup;
 pub mod telemetry;
 pub mod types;
+
+pub static ENV: once_cell::sync::Lazy<minijinja::Environment<'static>> =
+    once_cell::sync::Lazy::new(|| {
+        let mut env = minijinja::Environment::new();
+        env.set_source(minijinja::Source::from_path("templates"));
+        env
+    });

25bfa90aa21c29c89e6f582a148855c887fc822d
mwombeki <hackerlubex@protonmail.com>
2023-07-07 14:36:53 +0300

    commit

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -7,6 +7,6 @@ pub mod types;
 pub static ENV: once_cell::sync::Lazy<minijinja::Environment<'static>> =
     once_cell::sync::Lazy::new(|| {
         let mut env = minijinja::Environment::new();
-        env.set_source(minijinja::Source::from_path("templates"));
+        env.add_template("templates");
         env
     });

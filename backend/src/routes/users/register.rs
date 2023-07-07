7948134f3bd9c4e0d722c40711568dae1d62a52a
mwombeki <hackerlubex@protonmail.com>
2023-07-07 22:54:44 +0300

    commit

--- backend/src/routes/users/register.rs
+++ backend/src/routes/users/register.rs
@@ -15,3 +15,17 @@ pub struct CreateNewUser {
     first_name: String,
     last_name: String,
 }
+
+#[tracing::instrument(name = "Adding a new user",
+skip(pool, new_user, redis_pool),
+fields(
+    new_user_email = %new_user.email,
+    new_user_first_name = %new_user.first_name,
+    new_user_last_name = %new_user.last_name
+))]
+#[actix_web::post("/register/")]
+pub async fn register_user(
+    pool: actix_web::web::Data<sqlx::postgres::PgPool>,
+    new_user: actix_web::web::Json<NewUser>,
+    redis_pool: actix_web::web::Data<deadpool_redis::Pool>,
+)
